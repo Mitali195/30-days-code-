ba3851484ad64afaad4d01e04c601ac034c63b90
Mitali Jadhav <67255177+Mitali195@users.noreply.github.com>
2022-01-26 22:09:53 +0530

    Create 15jan1.py

---
+++ 15jan1.py
@@ -0,0 +1,7 @@
+#WAP  to check wheather the eac word in astrng begins with upper letter or not.If all the word begins with upper letter print True otherwise print False:
+
+a = "I Like Python"
+if a ==a.title():
+    print("True")
+else:
+    print("False")   
